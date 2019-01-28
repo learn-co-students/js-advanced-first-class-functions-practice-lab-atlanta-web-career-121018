bbf7e608ad7bd8386970914c29ada6dd60330757
Vanessa Prema <vanessa.prema@flatironschool.com>
2019-01-28 17:38:18 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+function logDriverNames(drivers) {
+    return drivers.forEach(function(driver){
+        console.log(driver.name);
+    })
+} 
+
+function logDriversByHometown(drivers, htown) {
+    return drivers.forEach(function(driver){
+        if (driver.hometown === htown)
+            console.log(driver.name);
+    })
+}
+
+function driversByRevenue(drivers) {
+    return drivers.slice().sort(function(a,b){
+        return a.revenue - b.revenue;
+    })
+}
+
+function driversByName(drivers) {
+    return drivers.slice().sort(function(a,b){
+        return a.name.localeCompare(b.name)
+    })
+} 
+
+function totalRevenue(drivers) {
+    return drivers.reduce(function(total,current){
+       return total + current.revenue
+    },0)
+} 
+ // 0 = initial value --> reduce(fn,initialvalue)
+
+function averageRevenue(drivers) {
+    return totalRevenue(drivers)/drivers.length
+} 
