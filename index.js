09b5b9818153eec32e9c31aafe77067e0dce1316
Sarah Yun <sarahyun7@gmail.com>
2019-06-12 15:04:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solution in this file!
+// Code your solution in this file! 
+function logDriverNames(drivers) {
+    return drivers.forEach(function(driver) {
+        console.log(driver.name)
+    })
+}
+
+function logDriversByHometown(drivers, location) {
+    return drivers.forEach(function(driver) {
+        if (driver.hometown === location) {
+            console.log(driver.name)
+        }
+    })
+}
+
+function driversByRevenue(drivers) {
+    return drivers.slice().sort(function(a,b) {
+        return a.revenue - b.revenue
+    })
+}
+
+function driversByName(drivers) {
+    return drivers.slice().sort(function(a, b) {
+        return a.name.localeCompare(b.name);
+    })
+}
+
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+function totalRevenue(drivers) {
+    return drivers.reduce(function(accumulator, currentValue) {
+        return accumulator + currentValue.revenue
+    }, 0)
+}
+
+function averageRevenue(drivers) {
+    return (totalRevenue(drivers)/drivers.length);
+}
